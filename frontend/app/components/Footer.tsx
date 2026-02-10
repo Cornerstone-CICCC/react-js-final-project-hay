61017348f647408095aa5d47cfd54ade42187503
Yuna41 <uncnprs@gmail.com>
2026-02-10 11:17:11 -0800

    Fixed Footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className='border-t-5 border-[#B5EBE6] pb-4 text-sm'>
+    <footer className="border-t-5 border-[#B5EBE6] pb-4 text-sm">
       <div className="flex gap-8 justify-between flex-col px-4 py-6 md:flex-row md:max-w-5xl md:mx-auto md:pt-10 md:pb-8">
         <ul className="flex gap-3 flex-col md:flex-row md:gap-7">
           <li>
@@ -17,15 +17,15 @@ const Footer = () => {
           </li>
         </ul>
         <div>
-          <p className='underline'>We accept the following payment options.</p>
+          <p className="underline">We accept the following payment options.</p>
           <ul className="flex gap-3 items-center md:gap-4">
-            <li className='w-[44] md:w-[66]'>
+            <li className="w-[44] md:w-[66]">
               <Image src="/assets/footer/visa.png" alt="VISA" width={66} height={67} />
             </li>
-            <li className='w-[32] md:w-[49]'>
+            <li className="w-[32] md:w-[49]">
               <Image src="/assets/footer/master.png" alt="master" width={49} height={31} />
             </li>
-            <li className='w-[62] md:w-[78]'>
+            <li className="w-[62] md:w-[78]">
               <Image
                 src="/assets/footer/american-express.png"
                 alt="AMERICAN EXPRESS"
@@ -33,7 +33,7 @@ const Footer = () => {
                 height={78}
               />
             </li>
-            <li className='w-[32] md:w-[49]'>
+            <li className="w-[32] md:w-[49]">
               <Image src="/assets/footer/apple-pay.png" alt="Apply Pay" width={49} height={48} />
             </li>
           </ul>
