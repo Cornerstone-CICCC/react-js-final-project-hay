ff4ba7cd01115cde6cd80a2367dba2727703633c
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2026-02-12 10:40:13 -0800

    push to compare

--- frontend/app/products/[id]/page.tsx
+++ frontend/app/products/[id]/page.tsx
@@ -13,6 +13,9 @@ type Props = {
 const page = async ({ params }: Props) => {
   const { id } = await params;
 
+  const res = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/products/`)
+
+
   //dummy -> change to fetch by id
   const item = {
     _id: id,
