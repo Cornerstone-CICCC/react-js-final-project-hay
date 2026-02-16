ee760108eee1d270f30674bbb1f384a8bb22e700
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2026-02-16 10:58:54 -0800

    investigating stripe issue

--- frontend/app/checkout/page.tsx
+++ frontend/app/checkout/page.tsx
@@ -1,8 +1,9 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { stripe } from '../lib/stripe';
 import CheckoutForm from './components/CheckoutForm';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
 const page = async () => {
   const calculateOrderAmount = () => {
     return 1400;
