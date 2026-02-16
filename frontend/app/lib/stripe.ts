c04bdc2fa0f055430ae15be5676e02f186ca60d1
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2026-02-16 10:41:13 -0800

    push to update vercel

--- frontend/app/lib/stripe.ts
+++ frontend/app/lib/stripe.ts
@@ -2,7 +2,4 @@ import 'server-only';
 
 import Stripe from 'stripe';
 
-console.log('🔑 Secret key value:', process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
-console.log('🔑 Is it defined?', !!process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
-
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
