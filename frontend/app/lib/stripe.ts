7df60057683d244b825a3fff5124e454afdf4029
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2026-02-16 10:07:37 -0800

    stripe env name updated

--- frontend/app/lib/stripe.ts
+++ frontend/app/lib/stripe.ts
@@ -2,4 +2,7 @@ import 'server-only';
 
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string);
+console.log('🔑 Secret key value:', process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+console.log('🔑 Is it defined?', !!process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
